3b887233fd96908c3d4f0a69dee009c1e6b6b205
beatricebock <explaintotris@gmail.com>
2020-09-28 18:03:49 +0800

    api works! + styling

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,8 +1,30 @@
 var url =
+  "https://cors-anywhere.herokuapp.com/" +
   "http://newsapi.org/v2/top-headlines?" +
   "country=us&" +
   "apiKey=d02f619cf302402ea12c43f90bbc2139";
-var req = new Request(url);
-fetch(req).then(function (response) {
-  console.log(response.json());
-});
+
+let newsLink = document.getElementById("api-link");
+let newsImage = document.getElementById("api-image");
+
+document.getElementById("api-headline").innerText = "loading...";
+
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    // Work with JSON data here
+    let apiNews = data.articles[0];
+
+    document.getElementById("api-headline").innerText = apiNews.title;
+    document.getElementById("api-content").innerText = apiNews.description;
+
+    newsLink.setAttribute("href", apiNews.url);
+    newsImage.setAttribute("src", apiNews.urlToImage);
+    console.log(apiNews.title + apiNews.description);
+  })
+  .catch((err) => {
+    // Do something for an error here
+    console.log("error with API");
+  });
